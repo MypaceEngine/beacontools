6ff00d04f8b9ac65442b35bf82f84da4082e177e
MypaceEngine <spin.the.earth@gmail.com>
2019-04-23 21:43:15 +0900

    Fix bug

--- examples/scanner_eddystone_ibeacon_inout_example.py
+++ examples/scanner_eddystone_ibeacon_inout_example.py
@@ -1,37 +1,33 @@
 import time
+import datetime
+import sys
 from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneFilter, EddystoneUIDFrame, BeaconScanner, IBeaconFilter
 bt_list={}
 
 def callback(bt_addr, rssi, packet, additional_info):
-#    print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
-    id="Type:Unkonown Adress:"+bt_addr
-    if("namespace" in additional_info):
-        id="Type:Eddystone Adress:"+bt_addr+" Namespace:"+additional_info["namespace"]+" Instance:"+additional_info["instance"]
-    elif("uuid" in additional_info):
-        id="Type:iBeacon Adress:"+bt_addr+" UUID:"+additional_info["uuid"]+" Major:"+str(additional_info["major"])+" Minor:"+str(additional_info["minor"])
-    print("<Receive> %s " % (id))
-    if not (id in bt_list):
-        print("<IN> %s " % (id))
-    currentCPUTime=time.perf_counter()
-    bt_list[id]=currentCPUTime
-    
-scanner=None
-scannerStartTime=0;
+    try:
+        id="Type:Unkonown Adress:"+bt_addr
+        if("namespace" in additional_info):
+            id="Type:Eddystone Adress:"+bt_addr+" Namespace:"+additional_info["namespace"]+" Instance:"+additional_info["instance"]
+        elif("uuid" in additional_info):
+            id="Type:iBeacon Adress:"+bt_addr+" UUID:"+additional_info["uuid"]+" Major:"+str(additional_info["major"])+" Minor:"+str(additional_info["minor"])
+        print("<Receive> %s " % (id))
+        if not (id in bt_list):
+            print("<IN> %s " % (id))
+        currentCPUTime=time.perf_counter()
+        bt_list[id]=currentCPUTime
+    except:
+        print(sys.exc_info())
+
+# scan for all TLM frames of beacons
+scanner = BeaconScanner(callback)
+scanner.start()
 
 try:
     while True:
-        if(scanner==None)or(time.perf_counter()-scannerStartTime>3600):
-            print("<Beacon Scanner Restart>")
-            if not (scanner==None):
-                scanner.stop()
-            # scan for all TLM frames of beacons
-            scanner = BeaconScanner(callback)
-            scanner.start()
-            scannerStartTime=time.perf_counter()
         if(len(bt_list)==0):
             print("<No Listing>")
         for i in list(bt_list):
-            
             #Wait 30s for detecting to exit beacon.
             if(time.perf_counter()-bt_list[i]>30):
                 print("<OUT> %s " % (i))
--- examples/scanner_eddystone_inout_example.py
+++ examples/scanner_eddystone_inout_example.py
@@ -3,30 +3,25 @@ from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneFilter, Eddys
 bt_list={}
 
 def callback(bt_addr, rssi, packet, additional_info):
-#    print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
-    id="namespace:"+additional_info["namespace"]+" Instance:"+additional_info["instance"]
-    print("<Receive> %s " % (id))
-    if not (id in bt_list):
-        print("<IN> %s " % (id))
-    currentCPUTime=time.perf_counter()
-    bt_list[id]=currentCPUTime
-    
-scanner=None
-scannerStartTime=0;
+    try:
+        id="namespace:"+additional_info["namespace"]+" Instance:"+additional_info["instance"]
+        print("<Receive> %s " % (id))
+        if not (id in bt_list):
+            print("<IN> %s " % (id))
+        currentCPUTime=time.perf_counter()
+        bt_list[id]=currentCPUTime
+    except:
+        print(sys.exc_info())
+        
+# scan for all TLM frames of beacons in the namespace "01020304050607080910"
+scanner = BeaconScanner(callback, 
+    device_filter=EddystoneFilter(namespace="01020304050607080910"),
+    packet_filter=[EddystoneTLMFrame, EddystoneUIDFrame]
+)
+scanner.start()
 
 try:
     while True:
-        if(scanner==None)or(time.perf_counter()-scannerStartTime>3600):
-            print("<Beacon Scanner Restart>")
-            if not (scanner==None):
-                scanner.stop()
-            # scan for all TLM frames of beacons in the namespace "01020304050607080910"
-            scanner = BeaconScanner(callback, 
-                device_filter=EddystoneFilter(namespace="01020304050607080910"),
-                packet_filter=[EddystoneTLMFrame, EddystoneUIDFrame]
-            )
-            scanner.start()
-            scannerStartTime=time.perf_counter()
         for i in list(bt_list):
             #Wait 30s for detecting to exit beacon.
             if(time.perf_counter()-bt_list[i]>30):
